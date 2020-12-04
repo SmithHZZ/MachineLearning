0a005141dd4b3a86af85dd32d2a00ba601b35e3e
JsonZ <huzz@zumaxmedical.com>
2020-12-04 17:39:36 +0800

    快速构建网络、保存和导入、批训练

---
+++ pytorch/07_batch_train.py
@@ -0,0 +1,28 @@
+import torch
+from torch.autograd import Variable
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+
+# 批训练
+
+BATCH_SIZE = 5
+
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
+
+torch_dataset = data.TensorDataset(x, y)
+
+loader = data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=2
+)
+
+for epoch in range(3):
+    for step, (batch_x, batch_y) in enumerate(loader):
+        print('Epoch: ', epoch, '| Step:', step, '| batch x:',
+              batch_x.numpy(), '| batch y:', batch_y.numpy())
+
+
+
